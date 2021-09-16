f1ec64fd7a497907b2ddc50fc907657d1bff1731
Edson Junior <ejr_04@outlook.com>
2021-09-16 16:13:10 -0300

    Back-End iniciado

---
+++ Back-End/senai.hroads.webApi/senai.hroads.webApi/Domains/Tipohabilidade.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace senai.hroads.webApi.Domains
+{
+    public partial class Tipohabilidade
+    {
+        public Tipohabilidade()
+        {
+            Habilidades = new HashSet<Habilidade>();
+        }
+
+        public byte IdTipoHabilidade { get; set; }
+        public string NomeTipoHabilidade { get; set; }
+
+        public virtual ICollection<Habilidade> Habilidades { get; set; }
+    }
+}
